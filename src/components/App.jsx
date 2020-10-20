3a95bcc72ad438effca640b802866ed868bacb88
Maorguez <avila.mauro@gmail.com>
2020-10-20 02:41:22 -0500

    Implement Route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import '../assets/styles/styles.scss';
+import Layout from './Layout';
+import Home from './pages/Home';
 
-const App = () => <h1>MusicApp</h1>;
+const App = () => (
+  <BrowserRouter>
+    <Layout>
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </Layout>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './Header';
+import Menu from './Menu';
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Menu />
+    </>
+  );
+}
+
+export default Layout;
