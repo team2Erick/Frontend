46a4a1b14c4aba1f6ba58a1e3816cfa2ab0a90ef
Maorguez <avila.mauro@gmail.com>
2020-10-24 14:25:02 -0500

    components mainSlider, MusicItem, ScrollSlider, Player and table

--- src/components/Playbar.jsx
+++ src/components/Playbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import '../assets/styles/components/Playbar.scss';
+import Player from './Player/Player';
+/* import '../assets/styles/components/Playbar.scss'; */
 
 const Playbar = () => (
   <section className="playbar">
-    <h1>Playbar</h1>
+    <Player />
   </section>
 );
 
---
+++ src/components/datos/MusicList.jsx
@@ -0,0 +1,46 @@
+const MusicList = [
+  {
+    image:
+      'https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png',
+    title: 'Esta es una canción',
+    subtitle: 'Artista',
+  },
+  {
+    image:
+      'https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png',
+    title: 'Esta es una canción',
+    subtitle: 'Artista',
+  },
+  {
+    image:
+      'https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png',
+    title: 'Esta es una canción',
+    subtitle: 'Artista',
+  },
+  {
+    image:
+      'https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png',
+    title: 'Esta es una canción',
+    subtitle: 'Artista',
+  },
+  {
+    image:
+      'https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png',
+    title: 'Esta es una canción',
+    subtitle: 'Artista',
+  },
+  {
+    image:
+      'https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png',
+    title: 'Esta es una canción',
+    subtitle: 'Artista',
+  },
+  {
+    image:
+      'https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png',
+    title: 'Esta es una canción',
+    subtitle: 'Artista',
+  },
+];
+
+export default MusicList;
