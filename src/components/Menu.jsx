88f2efa76621592d843fb70a75dbd852e8807f14
Maorguez <avila.mauro@gmail.com>
2020-10-23 22:19:07 -0500

    Implement modal and functionality the add a new playlist

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import SidebarItems from './SidebarItems';
 import MyTracks from './MyTracks';
+import Modal from './Modal';
 import '../assets/styles/components/Menu.scss';
 
 const Menu = () => {
   const [state, setState] = useState({
     currentPlayList: 'home',
+    modal: false,
+    newPlaylist: {
+      /* rock: new Set(), */
+    },
   });
+
+  const newPlaylistRef = useRef(null);
+  const newPlaylist = Object.keys(state.newPlaylist);
+
+  const addNewPlaylist = e => {
+    e.preventDefault();
+    const list = newPlaylistRef.current.value;
+
+    setState({
+      ...state,
+      modal: false,
+      newPlaylist: { ...state.newPlaylist, [list]: new Set() },
+    });
+  };
+
   return (
-    <section>
+    <section className="container__menu">
       <figure>
         <img src="/src/assets/images/icon.svg" alt="icon" />
       </figure>
@@ -48,9 +68,45 @@ const Menu = () => {
       </div>
       <div className="menu">
         <ul className="menu__list">
-          <li>
+          <li
+            className="new-playlist"
+            onClick={() => {
+              setState({ ...state, modal: true })
+            }}
+          >
             <h3 className="menu__subtitle"> New Playlist</h3>
           </li>
+          {newPlaylist.map((item) => (
+            <li
+              key={item}
+              className={item === state.currentPlayList ? 'active' : ''}
+              onClick={() => {
+                setState({ ...state, currentPlayList: item })
+              }}
+            >
+              {item}
+            </li>
+          ))}
+          <Modal
+            show={state.modal}
+            close={() => {
+            setState({ ...state, modal: false });
+            }}
+          >
+            <form onSubmit={addNewPlaylist}>
+              <div className="title">Create a New PlayList</div>
+              <div className="content-wrap">
+                <input
+                  type="text"
+                  placeholder="My Playlist"
+                  required
+                  ref={newPlaylistRef}
+                />
+                <br />
+                <button type="submit">create</button>
+              </div>
+            </form>
+          </Modal>
         </ul>
       </div>
     </section>
