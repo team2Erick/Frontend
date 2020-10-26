4dbe2e5e2ca0aac31116d69af2fed07f5b1a93cd
Maorguez <avila.mauro@gmail.com>
2020-10-25 20:28:24 -0500

    ok

--- src/components/HomeLayout/HomeLayout.jsx
+++ src/components/HomeLayout/HomeLayout.jsx
@@ -1,39 +1,9 @@
-import React, { createContext, useReducer } from 'react';
+import React from 'react';
+
 import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
-<<<<<<< HEAD
-import Playbar from './components/Playbar/Playbar';
-import media from '../../media.json';
-
-export const Context = createContext(null);
-const DEFAULT_PLAYLIST = 'home';
-
-const initialState = {
-  media,
-  currentPlaylist: DEFAULT_PLAYLIST,
-  newPlaylist: {},
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'ADD_PLAYLIST':
-      return {
-        ...state,
-        newPlaylist: { ...state.newPlaylist, [action.newPlaylist]: new Set() },
-      };
-    case 'SET_PLAYLIST':
-      return { ...state, currentPlaylist: action.newPlaylist };
-    default:
-      break;
-  }
-  return state;
-};
-
-const Layout = ({children}) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-=======
 import Player from '../Player/Player';
-import ScrollSlider from "../ScrollSlider/ScrollSlider"
+import ScrollSlider from "../ScrollSlider/ScrollSlider";
 
 const playlist = [
   {
@@ -66,28 +36,16 @@ const playlist = [
     title: "Nombre de la canción 5",
     artist: "Nombre del artista 5"
   }
-]
->>>>>>> develop
+];
 
+const Layout = ({ children }) => {
   return (
     <>
-<<<<<<< HEAD
-      <Context.Provider value={{ state, dispatch }}>
-        <Header />
-        <Menu />
-        {children}
-        <Playbar />
-      </Context.Provider>
-=======
       <Header />
       <ScrollSlider title="test" items={playlist} />
-
-      {/* {children} */}
+      {children}
       <Menu />
       <Player playlist={playlist} />
-
-
->>>>>>> develop
     </>
   );
 };
