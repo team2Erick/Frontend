c53436472e8eb3afd71e8c0dbc7180aa98289cbe
Emiliano Pamont <emipmttt@gmail.com>
2020-10-26 21:16:25 -0600

    .

--- src/components/HomeLayout/HomeLayout.jsx
+++ src/components/HomeLayout/HomeLayout.jsx
@@ -4,6 +4,39 @@ import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
 import Player from '../Player/Player';
 
+const playlist = [
+  {
+    image: "https://homepages.cae.wisc.edu/~ece533/images/pool.png",
+    song: "/src/components/HomeLayout/test.mp3",
+    title: "Nombre de la canción 1",
+    artist: "Nombre del artista 1"
+  },
+  {
+    image: "https://homepages.cae.wisc.edu/~ece533/images/pool.png",
+    song: "/src/components/HomeLayout/test2.mp3",
+    title: "Nombre de la canción 2",
+    artist: "Nombre del artista 2"
+  },
+  {
+    image: "https://homepages.cae.wisc.edu/~ece533/images/pool.png",
+    song: "/src/components/HomeLayout/test.mp3",
+    title: "Nombre de la canción 3",
+    artist: "Nombre del artista 3"
+  },
+  {
+    image: "https://homepages.cae.wisc.edu/~ece533/images/pool.png",
+    song: "/src/components/HomeLayout/test.mp3",
+    title: "Nombre de la canción 4",
+    artist: "Nombre del artista 4",
+  },
+  {
+    image: "https://homepages.cae.wisc.edu/~ece533/images/pool.png",
+    song: "/src/components/HomeLayout/test.mp3",
+    title: "Nombre de la canción 5",
+    artist: "Nombre del artista 5"
+  }
+];
+
 const Layout = ({ children }) => {
   return (
     <>
