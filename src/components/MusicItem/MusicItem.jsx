c86a9c5b36c0e49b2c809c4acd9cefd290bcf295
Emiliano Pamont <emipmttt@gmail.com>
2020-10-27 23:36:38 -0600

    .

--- src/components/MusicItem/MusicItem.jsx
+++ src/components/MusicItem/MusicItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './MusicItem.scss'
 
-const MusicItem = ({ item, rounded, key, playlist }) => {
+const MusicItem = ({ item, rounded }) => {
     return (
         <div
             className={
