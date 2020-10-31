ec79b4009aa89896f60179fba4d41873bb5bcbfc
Maorguez <avila.mauro@gmail.com>
2020-10-31 08:27:50 -0500

    update/context

---
+++ src/hooks/useUser.js
@@ -0,0 +1,27 @@
+import { useState, useContext, useCallback } from 'react';
+import Context from '../store/userContext';
+import loginService from '../services/login';
+
+export default function useUser() {
+  const { jwt, setJWT } = useContext(Context);
+  const [state, setState] = useState();
+
+  const login = useCallback(
+    ({ email, password }) => {
+      loginService({ email, password }).then((jwt) => {
+        setJWT(jwt);
+      });
+    },
+    [setJWT]
+  );
+
+  const logout = useCallback(() => {
+    setJWT(null);
+  }, [setJWT]);
+
+  return {
+    isLogged: Boolean(jwt),
+    login,
+    logout,
+  };
+}
