d143a0306ccba7c5e0e99d1b1feaa191559ab81a
Emiliano Pamont <emipmttt@gmail.com>
2020-11-01 05:07:28 -0600

    update/state

--- src/pages/Albums/Albums.jsx
+++ src/pages/Albums/Albums.jsx
@@ -10,51 +10,41 @@ import api from '../../services/api';
 
 const Albums = () => {
   const [album, setAlbum] = useState([]);
-
-  // useEffect(async () => {
-  //   const albumQuery = await api.get("/music/album");
-
-  //   setAlbum(albumQuery.data.data)
-
-  // }, [])
   const [albumRamdom, setAlbumRamdom] = useState([]);
   const [albumPrimary, setAlbumPrimary] = useState({});
 
   useEffect(() => {
     async function fetchData() {
       const albumQuery = await api.get("/music/album");
-    setAlbumPrimary(albumQuery.data.data.splice(0,1))
-    setAlbumRamdom(albumQuery.data.data.sort(() => {return Math.random() - 0.5}))
-    console.log(albumRamdom[0])
+      setAlbumPrimary(albumQuery.data.data.splice(0, 1)[0])
+      setAlbumRamdom(albumQuery.data.data.sort(() => { return Math.random() - 0.5 }))
+      console.log(albumRamdom[0])
     }
     fetchData()
   }, [])
-  
+
 
   return (
     <Layout>
       {
         albumRamdom.length > 0 && (
           <section className="main albums">
-        <ScrollSlider title="Featured Tracks" items={albumRamdom} album={true} />
-        <div className="album">
-          <div className="album__image">
-            <h2>Kamelia</h2>
-            <div className="album__image__contentimg">
-                 <img src={Singer} />
+            <ScrollSlider title="Featured Tracks" items={albumRamdom} album={true} />
+            <div className="album">
+              <div className="album__image">
+                <h2>{albumPrimary.artist.name}</h2>
+                <div className="album__image__contentimg">
+                  <img src={albumPrimary.artist.picture_big} />
+                </div>
+              </div>
+              <div className="album__table">
+                <h4>// Album</h4>
+                <h1>{albumPrimary.title}</h1>
+                <h4>{albumPrimary.contributors.map(contributors => contributors.name).join(",")}</h4>
+                <Table playlist={albumPrimary.tracks.data} dense={true} />
+              </div>
             </div>
-          </div>
-          <div className="album__table">
-            <h4>// Album</h4>
-            <h1>Around The World</h1>
-            <h4>Akcent Feat. Lidia Buble & DDY Nunes</h4>
-            <p>
-              The artists we represent are one of the most successful in Romania and also were a huge breakthrough in the international market, topping radio and sales around the world.
-          </p>
-            <Table playlist={albumRamdom} />
-          </div>
-        </div>
-      </section>
+          </section>
         )
       }
     </Layout>
