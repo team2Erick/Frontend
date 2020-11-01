b643599d4f1f3bbcc7ece172dd1430f98f3eda1c
Emiliano Pamont <emipmttt@gmail.com>
2020-11-01 01:25:42 -0600

    update/api query with files

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,20 @@ const config = new axios.create({
     baseURL: 'https://cday-music.herokuapp.com/api/',
     // baseURL: 'http://localhost:3000/api/',
 })
+
+export const file = async (endpoint, data) => {
+
+    var formData = new FormData();
+
+    Object.keys(data).forEach((el) => {
+        console.log(el, data[el]);
+        formData.append(el, data[el]);
+    })
+
+    const response = await config.post(endpoint, formData);
+
+    return response;
+
+}
+
 export default config
