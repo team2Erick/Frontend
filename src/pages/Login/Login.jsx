b8b43ed30edbcab9938567cdd6f6aa238e6c1c48
Maorguez <avila.mauro@gmail.com>
2020-11-01 08:42:51 -0500

    update/login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,84 +1,96 @@
 import React, { useState, useContext } from 'react';
-import { useHistory } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Login.scss';
 
-import Store from "../../store";
+import Store from '../../store';
 
-<<<<<<< HEAD
-
-import api from "../../services/api"
+import api from '../../services/api';
 
 const Login = () => {
-    const { state, setState } = useContext(Store);
-=======
-import api from "../../services/api"
+  const { state, setState } = useContext(Store);
 
-const Login = () => {
+  const history = useHistory();
 
-const { state, setState } = useContext(Store);
+  useEffect(() => {
+    if (state.user.id) history.push('/');
+  }, []);
 
->>>>>>> 981d5a629393b20001a0be1522001fd36ab239d3
-    const history = useHistory()
-    
-    useEffect(() => {
-        if (state.user.id) history.push('/')
-    }, [])
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
+  const loginQuery = (e) => {
+    e.preventDefault();
 
-    const loginQuery = (e) => {
-        e.preventDefault()
+    api
+      .post('auth/login', {
+        email,
+        password,
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  };
 
-        api.post("auth/login", {
-            email,
-            password
-        }).then(response => {
-            console.log(response);
-        })
-
-
-    }
-
-    return (<section className="loginpage">
-        <div className="loginpage__container">
-            <div className="presentation">
-                <figure className="loginpage__logo">
-                    <img src="/src/assets/images/icons/cday.svg" alt="logo cday" />
-                </figure>
-                <h1 className="loginpage__title">Listening and watching anytime, anywhere</h1>
-                <h4 className="loginpage__secondary-title">The artists we represent are one of the most successful in Romania and also were a huge breakthrough.</h4>
-                <div className="cel">
-                    <img src="/src/assets/images/icons/user.svg" alt="user" />
-                </div>
-            </div>
-            <div className="user">
-                <article className="loginpage__form">
-                    <h3 className="loginpage__form__titleform">Welcome to cday The best music online</h3>
-                    <h4 className="loginpage__form__secondary-titleform">Subcribe to our page, is free!</h4>
-                    <form onSubmit={loginQuery} className="forminfo">
-                        <input type="email" required placeholder="Email" />
-                        <input type="password" required placeholder="Password" />
-                        <div className="forminfo__separator"></div>
-                        <button type="submit">LOG IN</button>
-
-                    </form>
-                    <h4>Don´t have an acount? create one <span><Link to="/signup">Sing up</Link></span></h4>
-                    <div className="inscription">
-                        <button >
-                            <img src="/src/assets/images/icons/google-icon.svg" alt="logo google" /><span>LOG IN WITH GOOGLE</span>
-                        </button>
-                        <button>
-                            <img src="/src/assets/images/icons/facebook-icon.svg" alt="logo facebook" /><span>LOG IN WITH FACEBOOK</span>
-                        </button>
-                    </div>
-
-                </article>
-
-            </div>
+  return (
+    <section className="loginpage">
+      <div className="loginpage__container">
+        <div className="presentation">
+          <figure className="loginpage__logo">
+            <img src="/src/assets/images/icons/cday.svg" alt="logo cday" />
+          </figure>
+          <h1 className="loginpage__title">
+            Listening and watching anytime, anywhere
+          </h1>
+          <h4 className="loginpage__secondary-title">
+            The artists we represent are one of the most successful in Romania
+            and also were a huge breakthrough.
+          </h4>
+          <div className="cel">
+            <img src="/src/assets/images/icons/user.svg" alt="user" />
+          </div>
         </div>
-    </section>)
-}
+        <div className="user">
+          <article className="loginpage__form">
+            <h3 className="loginpage__form__titleform">
+              Welcome to cday The best music online
+            </h3>
+            <h4 className="loginpage__form__secondary-titleform">
+              Subcribe to our page, is free!
+            </h4>
+            <form onSubmit={loginQuery} className="forminfo">
+              <input type="email" required placeholder="Email" />
+              <input type="password" required placeholder="Password" />
+              <div className="forminfo__separator"></div>
+              <button type="submit">LOG IN</button>
+            </form>
+            <h4>
+              Don´t have an acount? create one{' '}
+              <span>
+                <Link to="/signup">Sing up</Link>
+              </span>
+            </h4>
+            <div className="inscription">
+              <button>
+                <img
+                  src="/src/assets/images/icons/google-icon.svg"
+                  alt="logo google"
+                />
+                <span>LOG IN WITH GOOGLE</span>
+              </button>
+              <button>
+                <img
+                  src="/src/assets/images/icons/facebook-icon.svg"
+                  alt="logo facebook"
+                />
+                <span>LOG IN WITH FACEBOOK</span>
+              </button>
+            </div>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Login;
