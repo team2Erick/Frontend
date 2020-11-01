03ef219e3bd01e1ecd95f4f28d48a32a24b6214f
Erik Guevara <emgladino@gmail.com>
2020-11-01 15:25:59 -0500

    new component error 404

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import Layout from '../../components/Layout/Layout'
+import Layout from '../../components/Layout/Layout';
+import smolly404 from '../../assets/images/smolly404.svg';
+import elipse from '../../assets/images/Ellipse 4.svg';
 import './NotFound.scss';
 
-
 const NotFound = () => (
-    <>
+  <>
     <Layout>
-        <h1>ERROR 404</h1>
-        <h3>Sorry, Page Not Found</h3>
+      <div class="content_404">
+        {/* <h1>ERROR 404</h1> */}
+        <img src={elipse} alt="" />
+        <div className="content_404-image">
+          <h1>ERROR 404</h1>
+          <img src={smolly404} alt="" />
+        </div>
+        <h3>SORRY, THIS PAGE DOESN´T EXISTS</h3>
+      </div>
     </Layout>
-    </>
+  </>
 );
 
 export default NotFound;
